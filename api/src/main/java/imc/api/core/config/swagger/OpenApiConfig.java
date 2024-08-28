195748cab7b45d873df24e7b0fb7c36fcffb6fe0
krosbift <torifaoficial@gmail.com>
2024-08-28 17:11:56 -0500

    swagger implemented sucefully

---
+++ api/src/main/java/imc/api/core/config/swagger/OpenApiConfig.java
@@ -0,0 +1,16 @@
+package imc.api.core.config.swagger;
+
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@Configuration
+@OpenAPIDefinition(
+    info = @Info(
+        title = "IMC API",
+        version = "1.0",
+        description = "IMC API Documentation"
+    )
+)
+public class OpenApiConfig { }
--- api/src/main/java/imc/api/routes/example/ExampleController.java
+++ api/src/main/java/imc/api/routes/example/ExampleController.java
@@ -6,30 +6,37 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 import imc.api.routes.example.models.ExampleModel;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping("/example")
+@Tag(name = "Example", description = "Example routes")
 public class ExampleController {
 
   @Autowired
   private ExampleService exampleService;
 
   @GetMapping
+  @Operation(summary = "Find example", description = "Find an example by its ID")
   public List<ExampleModel> findExample(@RequestParam Long idExample) {
     return exampleService.findExample(idExample);
   }
 
   @PostMapping
+  @Operation(summary = "Create example", description = "Create a new example")
   public ExampleModel createExample(@RequestBody ExampleModel user) {
     return exampleService.createExample(user);
   }
 
   @PatchMapping("/{id}")
+  @Operation(summary = "Update example", description = "Update an example by its ID")
   public ExampleModel updateExample(@RequestBody ExampleModel user, @PathVariable Long id) {
     return exampleService.updateExample(user, id);
   }
 
   @DeleteMapping("/{id}")
+  @Operation(summary = "Delete example", description = "Delete an example by its ID")
   public Long deleteExample(@PathVariable Long id) {
     return exampleService.deleteExample(id);
   }
