edee298123ffff7a25905424f87d9e1e566fe9ad
krosbift <torifaoficial@gmail.com>
2024-09-08 11:37:18 -0500

    add change

--- api/src/main/java/imc/api/core/config/swagger/OpenApiConfig.java
+++ api/src/main/java/imc/api/core/config/swagger/OpenApiConfig.java
@@ -7,10 +7,10 @@ import io.swagger.v3.oas.annotations.info.Info;
 
 @Configuration
 @OpenAPIDefinition(
-    info = @Info(
-        title = "IMC API",
-        version = "1.0",
-        description = "IMC API Documentation"
-    )
+  info = @Info(
+    title = "IMC API",
+    version = "1.0",
+    description = "IMC API Documentation"
+  )
 )
 public class OpenApiConfig { }
